0d1b56e60dcf384decfbed0dd25b3f4a1c7a9ede
Mykhailo Vstavskyi <vstavskyi@users.noreply.github.com>
2020-11-22 08:27:51 +0200

    Add files via upload

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule Jamdb.Sybase.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :jamdb_sybase,
+     version: "0.7.4",
+     elixir: "~> 1.7",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
+     deps: deps()]
+  end
+
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.3"},
+      {:ex_doc, "~> 0.21", only: :docs}
+    ]
+  end
+
+  defp description do
+    "Erlang driver and Ecto adapter for Sybase"
+  end
+
+  defp package do
+    [files: ["src","include","lib","mix.exs"],
+    maintainers: ["Sergiy Kostyushkin","Mykhailo Vstavskyi"],
+    licenses: ["MIT"],
+    links: %{"Github" => "https://github.com/erlangbureau/jamdb_sybase"}]
+  end
+end
