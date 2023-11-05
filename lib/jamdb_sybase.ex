7ce8e33ba2026b9e3e4f0ba109b1dc5f0bf47217
Mykhailo Vstavskyi <vstavskyi@users.noreply.github.com>
2023-11-05 07:19:25 +0200

    Update jamdb_sybase.ex

--- lib/jamdb_sybase.ex
+++ lib/jamdb_sybase.ex
@@ -41,7 +41,7 @@ defmodule Jamdb.Sybase do
     * `:rows` - the result set as a list  
   """
   @spec query(conn :: any(), sql :: any(), params :: any()) ::
-    {:ok, any()} | {:error | :disconnect, any()}
+    {:ok, any(), new_state :: any()} | {:error | :disconnect, any(), new_state :: any()}
   def query(conn, sql, params \\ [])
   def query(%{conn: conn, timeout: timeout} = s, sql, params) do
     case sql_query(conn, sql, params, timeout) do
